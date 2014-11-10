84db7fc595dbb6d0804731891eef1764e7ea320b
Kevin GITTENS <kevin.gittens@y-nov.com>
2014-11-10 10:03:47 +0100

    Test Intégration EpicEditor

--- Sandbox/Test.php
+++ Sandbox/Test.php
@@ -3,4 +3,7 @@ echo ("Ceci est une page de test");
 ?>
 <div id="epiceditor">
 <script src="../EpicEditor/js/epiceditor.min.js"></script>
+<script type="text/javascript">
+var editor = new EpicEditor().load();
+</script>
 </div>
